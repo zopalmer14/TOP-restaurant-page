53e0d062f12a67860f43bf144c4fe399d1695d13
zopalmer14 <zopalmer14@gmail.com>
2024-03-07 13:12:05 -0500

    create home page content and add restaurant background image

--- dist/dynamic.js
+++ dist/dynamic.js
@@ -2,6 +2,9 @@
 function initializePage() {
     // setup the nav buttons to facilitate tabbed browsing
     setupNav();
+
+    // load the home 'tab' by default
+    loadHome();
 }
 
 function setupNav() {
@@ -34,10 +37,33 @@ function loadHome() {
     const page_content = document.querySelector('#content');
 
     // create div to test
-    const test = document.createElement('div');
-    test.textContent = 'home';
+    const home_container = document.createElement('div');
+    home_container.classList.add('info');
 
-    page_content.replaceChildren(test);
+    // first text section
+    const restaurant_intro = document.createElement('div');
+    restaurant_intro.textContent = `
+        Best cafe / cantina / restaurant east of the Columbia!
+        Founded in Portland all the way back in 1926!
+    `;
+
+    // cool image 
+    const cool_img = document.createElement('img');
+    cool_img.src = '../src/portland-sign.jpg';
+
+    // second text section
+    const restaurant_aside = document.createElement('div');
+    restaurant_aside.textContent = `
+        Order online or come visit us at our original
+        location in SE Hawthorne! 
+    `;
+
+    // append all to container
+    home_container.appendChild(restaurant_intro);
+    home_container.appendChild(cool_img);
+    home_container.appendChild(restaurant_aside);
+
+    page_content.replaceChildren(home_container);
 }
 
 function loadMenu() {
