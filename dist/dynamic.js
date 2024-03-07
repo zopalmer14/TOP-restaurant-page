0e81dd024eb4b4740c8eab56b2d46afd7b88050f
zopalmer14 <zopalmer14@gmail.com>
2024-03-07 17:01:27 -0500

    create menu layout and fill with dummy content

--- dist/dynamic.js
+++ dist/dynamic.js
@@ -4,7 +4,10 @@ function initializePage() {
     setupNav();
 
     // load the home 'tab' by default
-    loadHome();
+    //loadHome();
+
+    // TESTING
+    loadMenu();
 }
 
 function setupNav() {
@@ -38,7 +41,7 @@ function loadHome() {
 
     // create div to test
     const home_container = document.createElement('div');
-    home_container.classList.add('info');
+    home_container.classList.add('home-info');
 
     // first text section
     const restaurant_intro = document.createElement('div');
@@ -70,11 +73,50 @@ function loadMenu() {
     // dom references 
     const page_content = document.querySelector('#content');
 
-    // create div to test
-    const test = document.createElement('div');
-    test.textContent = 'menu';
+    // create div to contain the menu items in a grid format / structure
+    const menu_container = document.createElement('div');
+    menu_container.classList.add('menu-info');
 
-    page_content.replaceChildren(test);
+    for (let i = 0; i < 4; i++) {
+        // create a div to represent each individual menu item
+        const menu_item = document.createElement('div');
+        menu_item.classList.add('menu-item');
+
+        // create a container for the image and append the image to it
+        const img_container = document.createElement('div');
+        img_container.classList.add('img-container');
+
+        const item_img = document.createElement('img');
+        item_img.src = '../src/portland-sign.jpg';
+
+        img_container.appendChild(item_img);
+
+        // create a container for the item - contains the item name / title and a desc
+        const item_container = document.createElement('div');
+        item_container.classList.add('item-container');
+
+        const item_title = document.createElement('h2');
+        item_title.textContent = 'Test Menu Item';
+
+        const item_desc = document.createElement('div');
+        item_desc.classList.add('item-desc');
+        item_desc.textContent = `
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Necessitatibus fuga iure unde architecto numquam dolores fugiat velit . . .
+        `;
+
+        item_container.appendChild(item_title);
+        item_container.appendChild(item_desc);
+
+        // append the image container and item container to the menu_item
+        menu_item.appendChild(img_container);
+        menu_item.appendChild(item_container);
+
+        // append the menu_item to the container
+        menu_container.appendChild(menu_item);
+    }
+
+    page_content.replaceChildren(menu_container);
 }
 
 function loadAbout() {
